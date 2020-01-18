7931f759bf6eeb2e81a5cf72b6b1ab058045fdea
Sachin Kumar Singh <singh.s.sachin@icloud.com>
2020-01-18 23:26:08 +0530

    Auth Done

--- Auth/models.py
+++ Auth/models.py
@@ -4,7 +4,7 @@ class authenticate(models.Model):
     name=models.CharField(max_length=50)
     uid=models.CharField(max_length=39)
     image=models.CharField(max_length=250)
-    email=models.ForeignKey('email',on_delete=models.SET_NULL)
+    email=models.ForeignKey('emails',on_delete=models.CASCADE)
     mobile=models.CharField(max_length=12)
     dob=models.CharField(max_length=12)
     regno=models.CharField(max_length=10)
@@ -13,3 +13,6 @@ class authenticate(models.Model):
     department=models.CharField(max_length=5)
     admin=models.BooleanField(default=False)
 
+class emails(models.Model):
+    email=models.CharField(max_length=250)
+    uid=models.CharField(max_length=39)
