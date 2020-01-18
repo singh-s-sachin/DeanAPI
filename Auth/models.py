36d4411a8e44d95de3901582cde5a3dd7f2369b1
Sachin Kumar Singh <singh.s.sachin@icloud.com>
2020-01-19 00:21:55 +0530

    Auth Done

--- Auth/models.py
+++ Auth/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 class authenticate(models.Model):
-    id = models.AutoField(primary_key=False)
     name=models.CharField(max_length=50)
     uid=models.CharField(max_length=39,editable=False)
     image=models.CharField(max_length=250)
-    email=models.CharField(max_length=100,primary_key=True)
+    email=models.Auto(max_length=100,primary_key=True)
     mobile=models.CharField(max_length=12)
     dob=models.CharField(max_length=12)
     regno=models.CharField(max_length=10,editable=False)
