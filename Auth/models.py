9f5f6988b798b0a251788f4da809c2fe8d9803f6
Sachin Kumar Singh <singh.s.sachin@icloud.com>
2020-01-18 23:35:15 +0530

    Auth Done

--- Auth/models.py
+++ Auth/models.py
@@ -1,18 +1,15 @@
 from django.db import models
 
 class authenticate(models.Model):
-    name=models.CharField(max_length=50)
-    uid=models.CharField(max_length=39)
-    image=models.CharField(max_length=250)
-    email=models.ForeignKey('emails',on_delete=models.CASCADE)
-    mobile=models.CharField(max_length=12)
-    dob=models.CharField(max_length=12)
-    regno=models.CharField(max_length=10)
-    created=models.CharField(max_length=10)
-    last_login=models.CharField(max_length=10)
-    department=models.CharField(max_length=5)
-    admin=models.BooleanField(default=False)
+    name=models.CharField(null=True,max_length=50)
+    uid=models.CharField(null=True,max_length=39,editable=False)
+    image=models.CharField(null=True,max_length=250)
+    email=models.CharField(max_length=100,primary_key=True)
+    mobile=models.CharField(null=True,max_length=12)
+    dob=models.CharField(null=True,max_length=12)
+    regno=models.CharField(null=True,max_length=10,editable=False)
+    created=models.CharField(null=True,max_length=10,editable=False)
+    last_login=models.CharField(null=True,max_length=10)
+    department=models.CharField(null=True,max_length=5)
+    admin=models.BooleanField(null=True,default=False)
 
-class emails(models.Model):
-    email=models.CharField(max_length=250)
-    uid=models.CharField(max_length=39)
