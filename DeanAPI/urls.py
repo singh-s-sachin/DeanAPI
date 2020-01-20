88044a100cc631c0e3a8aa0d042e3cccb7d24e0f
Sachin Kumar Singh <singh.s.sachin@icloud.com>
2020-01-20 22:58:53 +0530

    Secured

--- DeanAPI/urls.py
+++ DeanAPI/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('authenticate/', include('Auth.urls')),
     path('feeds/', include('posts.urls'))
 ]
