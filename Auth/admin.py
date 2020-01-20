5efa5d44152214b41fa3d04d4221cf1951edc8ce
Sachin Kumar Singh <singh.s.sachin@icloud.com>
2020-01-20 23:24:14 +0530

    Secured

--- Auth/admin.py
+++ Auth/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import authenticate
 # Register your models here.
 class authenticateAdmin(admin.ModelAdmin):
-    list_display = ('name','email','uid','dob','regno','created','department')
+    list_display = ('name','email','uid','regno','created','last_login')
 admin.site.register(authenticate, authenticateAdmin)
